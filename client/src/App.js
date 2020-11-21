533f3beb71f1a3bfdc240bf66c5e638e09891fcc
JOCECODE <ulah57@gmail.com>
2020-11-21 01:54:26 -0800

    CREATE HEADER COMPONENT

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,14 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import Header from "./Components/HeaderFolder/Header";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <>
+    <Header/>
+   </>
   );
 }
-
 
 export default App;
---
+++ client/src/Components/HeaderFolder/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+
+return  <div className="about-area">
+    <div className="container">
+    <div className="row">
+        <div className="col-lg-4">
+            <div className="about-img">
+                <img src="./pics/profile720.jpg" alt="Self-Portrait"/>
+            </div>
+        </div>
+        <div className="col-lg-6 mt-3 mt-md-5">
+         <div className="about-text">   
+             <h2>Jose Ulices Perez Jr.</h2>
+            <p>
+                A full-stack web developer from Norwalk, C.A. I enjoy making easy to use, fast and responsive websites.
+            </p>
+            <button className="profileButton" href="#">Learn More!</button>
+        </div>
+        </div>
+    </div>
+</div>
+<svg className="waveDiv" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+    <path fill="white" 
+    fillOpacity="1" 
+    d="M0,224L34.3,202.7C68.6,
+    181,137,139,206,154.7C274.3,
+    171,343,245,411,256C480,267,
+    549,213,617,170.7C685.7,128,
+    754,96,823,101.3C891.4,107,960,
+    149,1029,160C1097.1,171,1166,149,
+    1234,138.7C1302.9,128,1371,128,1406,
+    128L1440,128L1440,320L1405.7,320C1371.4,
+    320,1303,320,1234,320C1165.7,320,1097,320,
+    1029,320C960,320,891,320,823,320C754.3,320,
+    686,320,617,320C548.6,320,480,320,411,320C342.9,
+    320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+  </svg>
+</div>
+}
+
+export default Header;
