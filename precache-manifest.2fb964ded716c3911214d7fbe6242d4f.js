8bc002d7aa370a981316ffe102426ffd45d49cf7
JOCECODE <ulah57@gmail.com>
2020-11-21 23:55:55 -0800

    Updates

---
+++ precache-manifest.2fb964ded716c3911214d7fbe6242d4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6790253a1b2204501e622eb9c3a91c8c",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "6d9a0a8e2906622569e3",
+    "url": "/React-Portfolio/static/css/2.0e9b1384.chunk.css"
+  },
+  {
+    "revision": "ff4dd648b53c1a946eac",
+    "url": "/React-Portfolio/static/css/main.cd65c9b2.chunk.css"
+  },
+  {
+    "revision": "6d9a0a8e2906622569e3",
+    "url": "/React-Portfolio/static/js/2.6b4a18bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.6b4a18bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4dd648b53c1a946eac",
+    "url": "/React-Portfolio/static/js/main.6154fb3e.chunk.js"
+  },
+  {
+    "revision": "1097d1b20942e28c0a8e",
+    "url": "/React-Portfolio/static/js/runtime-main.31c074e0.js"
+  }
+]);
