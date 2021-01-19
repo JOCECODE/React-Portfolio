353f09f51ad8d89ed59983c6cb6b1f61e32debc3
JOCECODE <ulah57@gmail.com>
2021-01-18 18:12:51 -0800

    CREATE MP4 OF PROJECTS DISPLAYING CORRECTLY NEED TO STYLE AND EDIT THE VIDEOS;

--- client/src/Components/PortfolioFolder/Portfolio.js
+++ client/src/Components/PortfolioFolder/Portfolio.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Portfolio.css";
+import Vibee from "./assets/Vibee.mp4";
+import Bud from "./assets/BudjetTracker.mp4";
+import Pucman from "./assets/PucmanChase.mp4";
 
 function Portfolio() {
     return <>
@@ -14,21 +17,26 @@ function Portfolio() {
   </div>
   <div className="container projectsContainer">
         <div className="row">
-          <div className="col left centerThis" data-aos="zoom-in-left" data-aos-duration="2500">
-            <img src="https://i.imgur.com/hCVt35Q.jpg" alt="" height="280px" width="300px"/>
-            
+          <div id="noPadding" className="col left centerThis" data-aos="zoom-in-left" data-aos-duration="2500">
+          <video width="480" height="480" autoPlay muted >
+            <source src={Vibee} type="video/mp4"/>
+          </video>
           </div>
       </div>
       <div className="row">
-        <div className="col centerThis" data-aos="fade-up"
+        <div id="noPadding" className="col centerThis" data-aos="fade-up"
      data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
-          <img src="https://i.imgur.com/hCVt35Q.jpg" alt="" height="280px" width="300px"/>
+           <video width="480" height="480" autoPlay muted >
+            <source src={Bud} type="video/mp4"/>
+          </video>
           
         </div>
     </div>
     <div className="row">
-        <div className="col right centerThis" data-aos="zoom-in-right" data-aos-duration="1500">
-          <img src= "https://i.imgur.com/hCVt35Q.jpg" alt="" height="280px" width="300px"/>
+        <div  id="noPadding" className="col right centerThis" data-aos="zoom-in-right" data-aos-duration="1500">
+        <video width="480" height="480" autoPlay muted >
+            <source src={Pucman} type="video/mp4"/>
+          </video>
         </div>
     </div>
   </div>
