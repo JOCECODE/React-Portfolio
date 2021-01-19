2059a4e55df4cd60f626c292706c297c6252f756
JOCECODE <ulah57@gmail.com>
2021-01-18 22:00:31 -0800

    CREATE SKELETON DESIGN;

--- client/src/Components/PortfolioFolder/Portfolio.js
+++ client/src/Components/PortfolioFolder/Portfolio.js
@@ -3,6 +3,7 @@ import "./Portfolio.css";
 import Vibee from "./assets/Vibee.mp4";
 import Bud from "./assets/BudjetTracker.mp4";
 import Pucman from "./assets/PucmanChase.mp4";
+// import Col from "react-bootstrap/Col";
 
 function Portfolio() {
     return <>
@@ -15,18 +16,36 @@ function Portfolio() {
           </div>
       </div>
   </div>
+
+
   <div className="container projectsContainer">
         <div className="row">
-          <div id="noPadding" className="col left centerThis" data-aos="zoom-in-left" data-aos-duration="2500">
-          <video width="480" height="480" autoPlay muted >
+          
+         
+          <div className="col-xs-10">
+            <video className="video" width="360" height="360" autoPlay muted >
             <source src={Vibee} type="video/mp4"/>
           </video>
+
           </div>
+
+          <div id="noPadding" className="col-xs-2" data-aos="zoom-in-left" data-aos-duration="2500">
+            hello there I dont know why hey I dont know why I I run away!
+          </div>
+
+          {/* <Col xs={12} md={4}>
+          <p>
+            This is holding just a few amount of text before I can go to the mall!
+          </p>
+          </Col> */}
+      
       </div>
+
+
       <div className="row">
         <div id="noPadding" className="col centerThis" data-aos="fade-up"
      data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
-           <video width="480" height="480" autoPlay muted >
+           <video width="360" height="360" autoPlay muted >
             <source src={Bud} type="video/mp4"/>
           </video>
           
@@ -34,12 +53,14 @@ function Portfolio() {
     </div>
     <div className="row">
         <div  id="noPadding" className="col right centerThis" data-aos="zoom-in-right" data-aos-duration="1500">
-        <video width="480" height="480" autoPlay muted >
+        <video width="360" height="360" autoPlay muted >
             <source src={Pucman} type="video/mp4"/>
           </video>
         </div>
     </div>
   </div>
+    
+    
     </>
 }
 
