ba5407cda579a15ebc757e107b13f6b1d7d6ef46
JOCECODE <ulah57@gmail.com>
2021-02-05 22:36:59 -0800

    SECOND STYLING GOOD;

--- client/src/Components/PortfolioFolder/Portfolio.js
+++ client/src/Components/PortfolioFolder/Portfolio.js
@@ -3,6 +3,7 @@ import "./Portfolio.css";
 import Vibee from "./assets/Vibee.mp4";
 import Bud from "./assets/BudjetTracker.mp4";
 import Pucman from "./assets/PucmanChase.mp4";
+
 // import Col from "react-bootstrap/Col";
 
 function Portfolio() {
@@ -22,42 +23,55 @@ function Portfolio() {
         <div className="row">
           
          
-          <div className="col-xs-10">
+          <div className="col-4" data-aos="zoom-in-left" data-aos-duration="2500">
             <video className="video" width="360" height="360" autoPlay muted >
             <source src={Vibee} type="video/mp4"/>
           </video>
 
           </div>
 
-          <div id="noPadding" className="col-xs-2" data-aos="zoom-in-left" data-aos-duration="2500">
-            hello there I dont know why hey I dont know why I I run away!
+          <div id="noPadding" className="col-8 left" data-aos="zoom-in-left" data-aos-duration="2000">
+            <div>hello there I dont know why hey I dont know why I I run away!</div>
           </div>
 
-          {/* <Col xs={12} md={4}>
-          <p>
-            This is holding just a few amount of text before I can go to the mall!
-          </p>
-          </Col> */}
-      
       </div>
 
 
       <div className="row">
-        <div id="noPadding" className="col centerThis" data-aos="fade-up"
-     data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
+           <div id="noPadding" className="col-8" data-aos="zoom-in-right" data-aos-duration="2000">
+      <div className="right my-auto">
+             Budget Tracker application that also works offline using Webpack
+           </div>
+      </div>
+      
+      <div id="noPadding" className="col-4" data-aos="zoom-in-right" data-aos-duration="1000">
            <video width="360" height="360" autoPlay muted >
             <source src={Bud} type="video/mp4"/>
           </video>
-          
+
         </div>
+    
+   
+    
     </div>
-    <div className="row">
-        <div  id="noPadding" className="col right centerThis" data-aos="zoom-in-right" data-aos-duration="1500">
+   
+   
+   
+    <div className="row flexContainer">
+          <span className="pucText my-auto">
+            THIS IS TEXT FOR THE PUCMAN LAST ONE
+        </span>
+        
+        <div  id="noPadding" className="col right centerThis pucVideo" data-aos="zoom-in-right" data-aos-duration="1500">
         <video width="360" height="360" autoPlay muted >
             <source src={Pucman} type="video/mp4"/>
-          </video>
-        </div>
+          </video> 
+        </div>  
+
     </div>
+
+
+
   </div>
     
     
