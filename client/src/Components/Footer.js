9c2baf3d6dd445f9895a99c54ae4aa9a52400043
JOCECODE <ulah57@gmail.com>
2021-02-15 21:27:06 -0800

    CREATE OTHER ICONS FOR LINKS;

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -3,16 +3,37 @@ import "./Footer.css";
 import Cat from "./icons/githubNoBack.png";
 
 export default function Footer() {
+ 
+ const openLink = (url) => {
+  let newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+  if (newWindow) newWindow.opener = null
+}
+
  return (
   <>
-   <div className="footerContainer">
-    
-    <div id="tits" className="square">
-     <img id="big" className="kitty" src={Cat}/>
+   <div  className="footerContainer">
+
+
+
+
+    <div title="GitHub" onClick={() => openLink("https://github.com/JOCECODE")} className="square">
+     <img  alt="Github Logo"  className="kitty" src={Cat}/>
     </div>
+
+    <div title="LinkedIn" onClick={() => openLink("#")} className="square square2">
+     <img  alt="Github Logo"  className="kitty" src={Cat}/>
+    </div>
+
+    <div title="Other Link" onClick={() => openLink("https://github.com/JOCECODE")} className="square square3">
+     <img  alt="Github Logo"  className="kitty" src={Cat}/>
+    </div>
+
+
     <div className="bottomName">
      JOSE ULICES PEREZ JR. <span style={{color: "red"}}>@2021</span>
     </div >
+
+
    </div>
   </>
  )
